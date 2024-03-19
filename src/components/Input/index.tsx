27380f01ce19113debaf17aff9ba87016aff4137
ttaerrim <ltr0121@naver.com>
2024-03-19 22:53:43 +0900

    modify: Input 컴포넌트 추가

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,57 @@
+import { useState } from 'react';
+import styles from './Input.module.css';
+
+export default function Index({
+  value = '',
+  placeholder = '내용을 입력해 주세요.',
+  disabled = false,
+  readOnly = false,
+}: {
+  value?: string;
+  placeholder?: string;
+  disabled?: boolean;
+  readOnly?: boolean;
+}) {
+  const [input, setInput] = useState(value);
+  const [isBlurred, setIsBlurred] = useState(true);
+  const [buttonDisabled, setButtonDisabled] = useState(true);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.inputWrapper}>
+        <textarea
+          className={styles.input}
+          placeholder={placeholder}
+          value={input}
+          onChange={(e) =>
+            setInput((prev) => {
+              setButtonDisabled(prev === e.target.value);
+              return e.target.value;
+            })
+          }
+          rows={5}
+          onBlur={() => {
+            if (buttonDisabled) setIsBlurred(true);
+          }}
+          onFocus={() => setIsBlurred(false)}
+          disabled={disabled || readOnly}
+          readOnly={readOnly}
+        />
+        <span className={styles.count}>{input.length}</span>
+      </div>
+      {!isBlurred && (
+        <button
+          type='button'
+          className={styles.button}
+          disabled={buttonDisabled}
+          onClick={() => {
+            setIsBlurred(true);
+            setButtonDisabled(true);
+          }}
+        >
+          Save
+        </button>
+      )}
+    </div>
+  );
+}
--- src/pages/inputs.tsx
+++ src/pages/inputs.tsx
@@ -1,3 +1,12 @@
+import Input from '../components/Input';
+import styles from './inputs.module.css';
+
 export default function InputsPage() {
-  return <>input</>;
+  return (
+    <div className={styles.inputWrapper}>
+      <Input />
+      <Input disabled={true} placeholder='주문 요청 사항을 입력해 주세요.' />
+      <Input readOnly={true} value='주문 요청을 시도합니다.' />
+    </div>
+  );
 }
