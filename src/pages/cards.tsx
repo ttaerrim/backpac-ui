b14edaa4d50430d6bb9921118f48d75663023c72
ttaerrim <ltr0121@naver.com>
2024-03-20 10:58:14 +0900

    design: Card 반응형으로 수정

--- src/pages/cards.tsx
+++ src/pages/cards.tsx
@@ -6,11 +6,21 @@ const LOREM_IPSUM =
 
 export default function CardsPage() {
   return (
-    <div className={styles.cardContainer}>
-      <Card direction='column' label='Card Label' title='Card Title' star={3} description={LOREM_IPSUM} />
-      <Card direction='column' label='Card Label 2' title='Card Title 2' star={4} />
-      <Card direction='column' label='Card Label 3' title='Card Title 3' description={LOREM_IPSUM} />
-      <Card direction='row' label='Card Label 4' title={LOREM_IPSUM} description={LOREM_IPSUM} star={3} />
+    <div className={styles.container}>
+      <h2>세로형 카드</h2>
+      <div className={styles.rowCardWrapper}>
+        <Card direction='column' label='Card Label' title='Card Title' star={3} description={LOREM_IPSUM} />
+        <Card direction='column' label='Card Label 2' title='Card Title 2' star={4} />
+        <Card direction='column' label='Card Label 3' title='Card Title 3' description={LOREM_IPSUM} />
+        <Card direction='column' label='Card Label 4' title='Card Title 3' description={LOREM_IPSUM} />
+      </div>
+      <h2>가로형 카드</h2>
+      <div className={styles.columnCardWrapper}>
+        <Card direction='row' label='Card Label 1' title={LOREM_IPSUM} description={LOREM_IPSUM} star={3} />
+        <Card direction='row' label='Card Label 2' title={LOREM_IPSUM} description={LOREM_IPSUM} star={3} />
+        <Card direction='row' label='Card Label 3' title={LOREM_IPSUM} description={LOREM_IPSUM} star={3} />
+        <Card direction='row' label='Card Label 4' title={LOREM_IPSUM} description={LOREM_IPSUM} star={3} />
+      </div>
     </div>
   );
 }
