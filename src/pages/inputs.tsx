9686cee456787ef8948c421b5e0fa1121253280a
ttaerrim <ltr0121@naver.com>
2024-03-21 22:04:10 +0900

    modify: input 페이지 텍스트 추가

--- src/pages/inputs.tsx
+++ src/pages/inputs.tsx
@@ -4,8 +4,11 @@ import styles from './inputs.module.css';
 export default function InputsPage() {
   return (
     <div className={styles.inputWrapper}>
+      <h3>일반 Input</h3>
       <Input />
+      <h3>비활성화 Input</h3>
       <Input disabled={true} placeholder='주문 요청 사항을 입력해 주세요.' />
+      <h3>읽기전용 Input</h3>
       <Input readOnly={true} value='주문 요청을 시도합니다.' />
     </div>
   );
